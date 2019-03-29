ca746c50b1c7920cadf12699405ac56d1642b296
Paco Guevara <pacoguevaraa@gmail.com>
2019-03-29 09:10:27 -0600

    add database

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image')
 const db = knex ({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'tegik',
-    password : 'kopoyeba',
-    database : 'smartbrain'
+    host : process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
